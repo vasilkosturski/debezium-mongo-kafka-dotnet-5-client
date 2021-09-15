11de367853934cf52250c727ca196d2d071697c1
Vasil Kosturski <v.kosturski@synergysportstech.com>
2021-09-15 18:06:59 +0300

    Initial Kafka consumer implemented

--- src/DebeziumMongoKafkaDotNetClient/DebeziumMongoKafkaDotNetClient/Program.cs
+++ src/DebeziumMongoKafkaDotNetClient/DebeziumMongoKafkaDotNetClient/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using Confluent.Kafka;
 
 namespace DebeziumMongoKafkaDotNetClient
 {
@@ -6,7 +8,42 @@ namespace DebeziumMongoKafkaDotNetClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var conf = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using var consumerBuilder = new ConsumerBuilder<Ignore, string>(conf).Build();
+            consumerBuilder.Subscribe("mongo1.testdb.users");
+
+            var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) => {
+                e.Cancel = true; // prevent the process from terminating.
+                cts.Cancel();
+            };
+
+            try
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var cr = consumerBuilder.Consume(cts.Token);
+                        Console.WriteLine($"Consumed message '{cr.Message.Value}' at: '{cr.TopicPartitionOffset}'.");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Error occured: {e.Error.Reason}");
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // Ensure the consumer leaves the group cleanly and final offsets are committed.
+                consumerBuilder.Close();
+            }
         }
     }
 }
